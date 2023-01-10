821d48a8d0a3f11efabe6a009358bcac99d22edf
Eduardo <edumoraesdg@gmail.com>
2023-01-10 08:14:52 -0400

    Exemplo 7.3

---
+++ src/Exemplo_7_3.java
@@ -0,0 +1,7 @@
+public class Exemplo_7_3 {
+    static void multiplicar(double a, double b) { //Parâmetro formal
+        double res;
+        res = a * b;
+        System.out.println("Resultado= " + res);
+    }
+}
